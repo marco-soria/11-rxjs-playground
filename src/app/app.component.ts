38f6683062749274db27dbcd5130e2c73b374147
marco-soria <marco.soriaa7@gmail.com>
2025-06-09 17:00:45 -0500

    using a signal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import {
+  Component,
+  DestroyRef,
+  effect,
+  inject,
+  OnInit,
+  signal,
+} from '@angular/core';
 import { interval, map } from 'rxjs';
 
 @Component({
@@ -7,19 +14,30 @@ import { interval, map } from 'rxjs';
   templateUrl: './app.component.html',
 })
 export class AppComponent implements OnInit {
+  clickCount = signal(0);
   private destroyRef = inject(DestroyRef);
 
-  ngOnInit(): void {
-    // Update the current time every second
-    const subscription = interval(1000)
-      .pipe(map((val) => val * 2))
-      .subscribe({
-        next: (val) => console.log(val),
-      });
-    // Clean up the subscription when the component is destroyed
-    this.destroyRef.onDestroy(() => {
-      subscription.unsubscribe();
-      //console.log('Subscription cleaned up');
+  constructor() {
+    effect(() => {
+      console.log(`Clicked button: ${this.clickCount()} times`);
     });
+  }
+
+  ngOnInit(): void {
+    // // Update the current time every second
+    // const subscription = interval(1000)
+    //   .pipe(map((val) => val * 2))
+    //   .subscribe({
+    //     next: (val) => console.log(val),
+    //   });
+    // // Clean up the subscription when the component is destroyed
+    // this.destroyRef.onDestroy(() => {
+    //   subscription.unsubscribe();
+    //   //console.log('Subscription cleaned up');
+    // });
+  }
+
+  onClick() {
+    this.clickCount.update((count) => count + 1);
   }
 }
