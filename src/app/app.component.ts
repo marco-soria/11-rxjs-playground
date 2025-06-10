9714f3b0059e69788ba1747381e99e7e0ba45e25
marco-soria <marco.soriaa7@gmail.com>
2025-06-09 21:07:11 -0500

    converted signal to observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import {
   OnInit,
   signal,
 } from '@angular/core';
+import { toObservable } from '@angular/core/rxjs-interop';
 import { interval, map } from 'rxjs';
 
 @Component({
@@ -15,12 +16,16 @@ import { interval, map } from 'rxjs';
 })
 export class AppComponent implements OnInit {
   clickCount = signal(0);
+  clickCount$ = toObservable(this.clickCount);
+  // interval = signal(0);
+  // doubleInterval = computed(() => this.interval() * 2);
   private destroyRef = inject(DestroyRef);
 
   constructor() {
-    effect(() => {
-      console.log(`Clicked button: ${this.clickCount()} times`);
-    });
+    // effect(() => {
+    //   console.log(`Clicked button: ${this.clickCount()} times`);
+    // });
+    //toObservable(this.clickCount)
   }
 
   ngOnInit(): void {
@@ -35,6 +40,16 @@ export class AppComponent implements OnInit {
     //   subscription.unsubscribe();
     //   //console.log('Subscription cleaned up');
     // });
+
+    //setInterval(() => {
+    //   this.clickCount.update((count) => count + 1);
+    // }, 1000);
+    const subscription = this.clickCount$.subscribe({
+      next: (val) => console.log(`Clicked button: ${this.clickCount()} times`),
+    });
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
   }
 
   onClick() {
